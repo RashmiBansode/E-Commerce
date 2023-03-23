4c910a791ca0170451b6ddc38fe65d72d7349e35
RashmiBansode <rashmibansode86@gmail.com>
2023-03-23 15:57:46 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',component:HomeComponent
+},
+
+{
+  path:'s-auth',component:SellerAuthComponent
+},
+
+{
+  path:'addproduct',component:AddproductComponent
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HomeComponent } from './home/home.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SellerAuthComponent,
+    HomeComponent,
+    AddproductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
