bd0dce4ce80fc3f4717f3df0354911332aca4b55
RashmiBansode <rashmibansode86@gmail.com>
2023-03-30 16:51:13 +0530

    updated

--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -8,38 +8,41 @@ import Login, { Signup } from '../data-type';
   providedIn: 'root'
 })
 export class SellerService {
-  isSellerloggedIn=new BehaviorSubject<boolean>(false)
-isLoginError=new EventEmitter<boolean>(false)
-  constructor(private http:HttpClient,private router:Router) { }
+  isSellerloggedIn = new BehaviorSubject<boolean>(false)
+  isLoginError = new EventEmitter<boolean>(false)
+  constructor(private http: HttpClient, private router: Router) { }
 
-  userSign(data:Signup){
-    this.http.post("http://localhost:3000/seller",data,{observe:'response'}).subscribe((res:any)=>{
+  userSign(data: Signup) {
+    this.http.post("http://localhost:3000/seller", data, { observe: 'response' }).subscribe((res: any) => {
       this.isSellerloggedIn.next(true);
-      localStorage.setItem('seller',JSON.stringify(res.body))
-      this.router.navigate(['seller-home']); 
+      localStorage.setItem('seller', JSON.stringify(res.body))
+      this.router.navigate(['seller-home']);
     });
   }
+
+
+
+  userLogin(data: Login) {
+    console.warn(data);
+    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`, { observe: 'response' }).subscribe((res: any) => {
+      if (res && res.body && res.body.length) {
+        console.warn("user logged in")
+        localStorage.setItem('seller', JSON.stringify(res.body))
+        this.router.navigate(['seller-home']);
+      }
+      else {
+        console.warn(" logged in failed");
+        this.isLoginError.emit(true)
+      }
+    });
+  }
+
 
   reloadSeller(){
     if(localStorage.getItem('seller')){
       this.isSellerloggedIn.next(true);
       this.router.navigate(['seller-home']);
      }
-  }
-
-  userLogin(data:Login){
-   console.warn(data);
-   this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`,{observe:'response'}).subscribe((res:any)=>{
-      if(res && res.body && res.body.length ){
-console.warn("user logged in")
-localStorage.setItem('seller',JSON.stringify(res.body))
-this.router.navigate(['seller-home']); 
-      }
-      else{
-        console.warn(" logged in failed");
-        this.isLoginError.emit(true)
-      }
-    });
   }
 }
 
